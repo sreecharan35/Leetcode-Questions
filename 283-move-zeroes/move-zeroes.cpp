eae611143fb60b7b7ccefdf9d80b911004285743
G.Sree Charan <sreecharan8354@gmail.com>
2026-08-02 07:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,8 +5,7 @@ public:
         int i=0;
         int j=1;
         while(i<n and j<n){
-
-            if(nums[i]==0 and nums[j]!=0){
+            if(nums[i] ==0 and nums[j]!=0){
                 int temp=nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
@@ -15,13 +14,12 @@ public:
             }
             else if(nums[i]==0 and nums[j]==0){
                 j++;
-
-            }
-            else if(nums[i] !=0 and nums[j]!=0){
-                j++;
-                i++;
             }
             else if(nums[i]!=0 and nums[j]==0){
+                i++;
+                j++;
+            }
+            else if(nums[i] !=0 and nums[j] !=0){
                 i++;
                 j++;
             }
